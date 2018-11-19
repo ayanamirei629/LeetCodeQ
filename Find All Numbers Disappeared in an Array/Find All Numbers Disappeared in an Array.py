e370ff2e24851a80f088037f753b7ee62cc2659f
Yingge Hu <ayanamirei629@gmail.com>
2018-11-19 17:51:06 -0500

    Add files via upload

---
+++ Find All Numbers Disappeared in an Array/Find All Numbers Disappeared in an Array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        length = len(nums)
+        return list(set(range(1,length+1)).difference(set(nums)))

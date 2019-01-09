188e17d5173251a0cb43fa75587e705364f84574
Yingge Hu <ayanamirei629@gmail.com>
2019-01-09 18:58:00 -0500

    Add files via upload

---
+++ Sqrt(x)/Sqrt(x).py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        a = x
+        while a*a > x:
+            a=(a+x//a)//2
+        return a
+    
+    
+    
+a= Solution()
+print a.mySqrt(8)
